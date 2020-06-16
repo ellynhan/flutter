d33f8afab9c7bdf4eb093736df2ffd677eda8109
Jaewon Han <38302837+ellynhan@users.noreply.github.com>
2020-06-16 21:15:02 +0900

    Create Maptest2.dart

---
+++ Maptest2.dart
@@ -0,0 +1,101 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: MapPage(),
+));
+
+const double CAMERA_ZOOM = 13;
+const double CAMERA_TILT = 0;
+const double CAMERA_BEARING = 30;
+const LatLng SOURCE_LOCATION = LatLng(42.7477863, -71.1699932);
+const LatLng DEST_LOCATION = LatLng(42.6871386, -71.2143403);
+
+class MapPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MapPageState();
+}
+
+class MapPageState extends State<MapPage> {
+  Completer<GoogleMapController> _controller = Completer();
+  // this set will hold my markers
+  Set<Marker> _markers = {};
+  // this will hold the generated polylines
+  Set<Polyline> _polylines = {};
+  // this will hold each polyline coordinate as Lat and Lng pairs
+  List<LatLng> polylineCoordinates = [];
+  // this is the key object - the PolylinePoints
+  // which generates every polyline between start and finish
+  PolylinePoints polylinePoints = PolylinePoints();
+  String googleAPIKey = "AIzaSyCQBy8IK74-OPl-vCvvss7jhs6vJXW40zE";
+  // for my custom icons
+  BitmapDescriptor sourceIcon;
+  BitmapDescriptor destinationIcon;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    CameraPosition initialLocation = CameraPosition(
+        zoom: CAMERA_ZOOM,
+        bearing: CAMERA_BEARING,
+        tilt: CAMERA_TILT,
+        target: SOURCE_LOCATION);
+    return GoogleMap(
+        myLocationEnabled: true,
+        compassEnabled: true,
+        tiltGesturesEnabled: false,
+        markers: _markers,
+        polylines: _polylines,
+        mapType: MapType.normal,
+        initialCameraPosition: initialLocation,
+        onMapCreated: (GoogleMapController controller) {
+      _controller.complete(controller);
+      setMapPins();
+      setPolylines();
+    });
+  }
+
+  void setMapPins() {
+    setState(() {
+      // source pin
+      _markers.add(Marker(
+          markerId: MarkerId('sourcePin'),
+          position: SOURCE_LOCATION,
+          icon: sourceIcon));
+      // destination pin
+      _markers.add(Marker(
+          markerId: MarkerId('destPin'),
+          position: DEST_LOCATION,
+          icon: destinationIcon));
+    });
+  }
+  setPolylines() async {
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        googleAPIKey,
+        PointLatLng(SOURCE_LOCATION.latitude, SOURCE_LOCATION.longitude),
+        PointLatLng(DEST_LOCATION.latitude, DEST_LOCATION.longitude),
+        travelMode: TravelMode.driving,
+    );
+    print(result.points);
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    }
+    setState(() {
+      Polyline polyline = Polyline(
+          polylineId: PolylineId('poly'),
+          color: Color.fromARGB(255, 40, 122, 198),
+          points: polylineCoordinates
+      );
+      _polylines.add(polyline);
+    });
+  }
+}
